3cdc6093dc1e87eaff1939eb68e9ba37df34dd44
reagankan <reagankan@gmail.com>
2016-09-08 10:39:54 -0700

    Lab 3

---
+++ AP Lesson 3/Lab_03.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		//set up user intput
+		Scanner cin = new Scanner(System.in);
+		
+		System.out.println("Lab 3");
+		
+	
+	
+
+	}	
+}

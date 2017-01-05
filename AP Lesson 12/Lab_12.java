4d772a6fc104ff11b6bfed1b2818d48de63b1078
reagankan <reagankan@gmail.com>
2017-01-05 12:15:36 -0800

    finished lab 12

---
+++ AP Lesson 12/Lab_12.java
@@ -0,0 +1,278 @@
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.ArrayList;
+import java.util.Random;
+import static java.lang.Math.pow;
+import static java.lang.Math.abs;
+import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
+
+public class Lab_12
+{
+	//Instantiate Scanner class object
+	static Scanner cin = new Scanner(System.in);
+	//Instantiate Random class object
+	static Random random = new Random();
+	static Vector<String> exercise_name = new Vector<String>();
+	public static void main(String[]args)
+	{
+		System.out.println("Lab_12");
+		create_exercise_name();
+		
+		Ex_1 one = new Ex_1();
+		one.main();
+		pauseFor(3);
+		endl();
+		
+
+		Ex_2 two = new Ex_2();
+		two.main();	
+	}	
+	public static class Ex_1
+	{
+		private int timeHR, timeMIN, distMI;
+		private double mph;
+		//default constructor
+		public Ex_1()
+		{
+			timeHR = 0;
+			timeMIN = 0;
+			distMI = 0;
+			mph = 0;
+		}
+		//constructor
+		public Ex_1(int readinHRS, int readinMIN, int readinDist)
+		{
+			timeHR = readinHRS;
+			timeMIN = readinMIN;
+			distMI = readinDist;
+		}
+		
+		//modifiers
+		public void setTimeHR(int newHR)
+		{
+			timeHR = newHR;
+		}
+		public void setTimeMIN(int newMIN)
+		{
+			timeMIN = newMIN;
+		}
+		public void setDist(int newDIST)
+		{
+			distMI = newDIST;
+		}
+		
+		//accessors
+		public int getTimeHR()
+		{
+			return timeHR;
+		}
+		public int getTimeMIN()
+		{
+			return timeMIN;
+		}
+		public int getDist()
+		{
+			return distMI;
+		}
+		public double getMPH()
+		{
+			mph = Math.round(distMI / (timeHR + timeMIN / 60.0));
+			return mph;
+		}
+
+		public static void main()
+		{
+			int hour = 0, minutes = 0, distance = 0;
+			System.out.println("Enter Hours: ");
+			System.out.print(">>");
+			hour = cin.nextInt();
+				
+			System.out.println("Enter Minutes: ");
+			System.out.print(">>");
+			minutes = cin.nextInt();
+				
+			System.out.println("Enter Distance in miles: ");
+			System.out.print(">>");
+			distance = cin.nextInt();
+			
+			Ex_1 MilesPerHour = new Ex_1(hour, minutes, distance);
+			System.out.println(
+				MilesPerHour.getDist() + " miles in " + 
+				MilesPerHour.getTimeHR() + " hours and " + 
+				MilesPerHour.getTimeMIN() + " minutes = " + 
+				MilesPerHour.getMPH() + " mph."
+			);
+			
+			//second calculation
+			System.out.println("Enter New Hours: ");
+			System.out.print(">>");
+			MilesPerHour.setTimeHR(cin.nextInt());
+				
+			System.out.println("Enter Minutes: ");
+			System.out.print(">>");
+			MilesPerHour.setTimeMIN(cin.nextInt());
+				
+			System.out.println("Enter Distance in miles: ");
+			System.out.print(">>");
+			MilesPerHour.setDist(cin.nextInt());
+			
+			System.out.println(
+				MilesPerHour.getDist() + " miles in " + 
+				MilesPerHour.getTimeHR() + " hours and " + 
+				MilesPerHour.getTimeMIN() + " minutes = " + 
+				MilesPerHour.getMPH() + " mph."
+			);
+		}
+	}
+	public static class Ex_2
+	{
+		private int X1, X2, Y1, Y2;
+		private double distance;
+		//default constructor
+		public Ex_2()
+		{
+			X1 = 0;
+			Y1 = 0;
+			X2 = 0;
+			Y2 = 0;
+		}
+		//constructor
+		public Ex_2(int xOne, int yOne, int xTwo, int yTwo)
+		{
+			X1 = xOne;
+			Y1 = yOne;
+			X2 = xTwo;
+			Y2 = yTwo;
+		}
+		
+		//modifiers
+		public void setX1(int newX)
+		{
+			X1 = newX;
+		}
+		public void setY1(int newY)
+		{
+			Y1 = newY;
+		}
+		public void setX2(int newX)
+		{
+			X2 = newX;
+		}
+		public void setY2(int newY)
+		{
+			Y2 = newY;
+		}
+		
+		//accessors
+		public int getX1()
+		{
+			return X1;
+		}
+		public int getY1()
+		{
+			return Y1;
+		}
+		public int getX2()
+		{
+			return X2;
+		}
+		public int getY2()
+		{
+			return Y2;
+		}
+		public double getDistance()
+		{
+			distance = Math.sqrt((X2-X1)*(X2-X1) + (Y2-Y1)*(Y2-Y1));
+			return distance;
+		}
+
+		public static void main()
+		{
+			int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+			System.out.println("Enter x1: ");
+			System.out.print(">>");
+			x1 = cin.nextInt();
+				
+			System.out.println("Enter y1: ");
+			System.out.print(">>");
+			y1 = cin.nextInt();
+				
+			System.out.println("Enter x2: ");
+			System.out.print(">>");
+			x2 = cin.nextInt();
+			
+			System.out.println("Enter y2: ");
+			System.out.print(">>");
+			y2 = cin.nextInt();
+			
+			Ex_2 distObject = new Ex_2(x1, y1, x2, y2);
+			System.out.printf(
+				"distance = %-10.2f\n", distObject.getDistance()
+			);
+			
+			//second calculation
+			System.out.println("Enter x1: ");
+			System.out.print(">>");
+			distObject.setX1(cin.nextInt());
+				
+			System.out.println("Enter y1: ");
+			System.out.print(">>");
+			distObject.setY1(cin.nextInt());
+				
+			System.out.println("Enter x2: ");
+			System.out.print(">>");
+			distObject.setX2(cin.nextInt());
+			
+			System.out.println("Enter y2: ");
+			System.out.print(">>");
+			distObject.setY2(cin.nextInt());
+			
+			System.out.printf(
+				"distance = %-10.2f\n", distObject.getDistance()
+			);
+		}
+	}
+	public static void create_exercise_name()
+	{
+		exercise_name.add("Miles Per Hour");
+		exercise_name.add("Distance Between 2 Points");
+	}
+	public static void printHeading(int num)
+	{
+		endl();
+		System.out.println("Exercise " + num + ": " + exercise_name.get(num-1));
+		endl();
+	}
+	public static void endl()
+	{
+		System.out.println();
+	}
+	public static void msleep(int milli)
+	{
+		try{
+			TimeUnit.MILLISECONDS.sleep(milli);
+		} catch(InterruptedException ex){
+			//do nothing
+		}
+	}
+	public static void pauseFor(int sec)
+	{
+		System.out.print("Moving on in: ");
+		for(int i = sec; i >= 1; i--)
+		{
+			System.out.print(i);
+			msleep(250);
+			cout("...");
+			
+		}
+	}
+	public static void cout(String str)
+	{
+		for(int i = 0; i < str.length(); i++)
+		{
+			System.out.print(str.charAt(i));
+			msleep(250);
+		}
+	}
+}
